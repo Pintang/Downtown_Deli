1378ef57049f29ef260387be6ea5173a6a09c5e4
Tyler Lynch <Tyler Lynch@TYLERSPC>
2018-03-05 20:15:31 -0500

    Add project files.

---
+++ DowntownDeliProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DowntownDeliProject.Global" Language="C#" %>
---
+++ DowntownDeliProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DowntownDeliProject.Startup))]
+namespace DowntownDeliProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
